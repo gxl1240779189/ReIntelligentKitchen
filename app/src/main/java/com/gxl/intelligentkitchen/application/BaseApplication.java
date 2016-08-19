7c88ffc74513eb1b140148e9cd2adb51b9fea3d3
betterAndroider <1240779189@qq.com>
2016-08-19 16:45:01 +0800

    2016/8/19  1.修改启动的样式细节

--- app/src/main/java/com/gxl/intelligentkitchen/application/BaseApplication.java
+++ app/src/main/java/com/gxl/intelligentkitchen/application/BaseApplication.java
@@ -25,20 +25,7 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         sContext = getApplicationContext();
-        //设置BmobConfig：允许设置请求超时时间、文件分片上传时每片的大小、文件的过期时间(单位为秒)
-        BmobConfig config = new BmobConfig.Builder(this)
-                //设置appkey(必填)
-                .setApplicationId("6a695ae946f5ebfa7fffa6d5c8ee5a32")
-                //请求超时时间（单位为秒）：默认15s
-                .setConnectTimeout(30)
-                //文件分片上传时每片的大小（单位字节），默认512*1024
-                .setUploadBlockSize(1024 * 1024)
-                //文件的过期时间(单位为秒)：默认1800s
-                .setFileExpiration(5500)
-                .build();
-        Bmob.initialize(config);
         ActiveAndroid.initialize(this);
-        SMSSDK.initSDK(this, "c7ab0de860bc", "2f031be4660cf6d57be19df4ab57cf1f");
     }
 
     @Override
--- app/src/main/java/com/gxl/intelligentkitchen/ui/activity/WelcomeActivity.java
+++ app/src/main/java/com/gxl/intelligentkitchen/ui/activity/WelcomeActivity.java
@@ -8,21 +8,38 @@ import android.view.Window;
 
 import com.gxl.intelligentkitchen.R;
 
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobConfig;
+import cn.smssdk.SMSSDK;
+
 /**
  * 作者：GXL on 2016/8/3 0003
  * 博客: http://blog.csdn.net/u014316462
  * 作用：欢迎界面
  */
-public class WelcomeActivity extends Activity{
+public class WelcomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.welcome_activity_main);
+        //设置BmobConfig：允许设置请求超时时间、文件分片上传时每片的大小、文件的过期时间(单位为秒)
+        BmobConfig config = new BmobConfig.Builder(this)
+                //设置appkey(必填)
+                .setApplicationId("6a695ae946f5ebfa7fffa6d5c8ee5a32")
+                //请求超时时间（单位为秒）：默认15s
+                .setConnectTimeout(30)
+                //文件分片上传时每片的大小（单位字节），默认512*1024
+                .setUploadBlockSize(1024 * 1024)
+                //文件的过期时间(单位为秒)：默认1800s
+                .setFileExpiration(5500)
+                .build();
+        Bmob.initialize(config);
+        SMSSDK.initSDK(this, "c7ab0de860bc", "2f031be4660cf6d57be19df4ab57cf1f");
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(WelcomeActivity.this,HomeActivity.class));
+                startActivity(new Intent(WelcomeActivity.this, HomeActivity.class));
                 finish();
             }
         }, 2000);
